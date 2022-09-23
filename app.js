c992defa86ffe44af2fcbc618dd8dae33f86a2d7
Facundo M <105024960+Facu6M@users.noreply.github.com>
2022-09-23 02:46:41 -0300

    Update app.js

--- app.js
+++ app.js
@@ -31,6 +31,33 @@ gallery.addEventListener("click", function() {
     document.querySelector(".modal-gallery__backgroundd").style.display = "block";
 })
 
+// seleccionar otra foto en Gallery DESKTOP
+
+const foto1 = document.getElementById("m1")
+const foto2 = document.getElementById("m2")
+const foto3 = document.getElementById("m3")
+const foto4 = document.getElementById("m4")
+
+foto1.addEventListener("click", function() {
+    const imagen1 = document.querySelector(".modal-gallery__image-container")
+    imagen1.style.backgroundImage = " url(./images/image-product-1.jpg)";
+})
+
+foto2.addEventListener("click", function() {
+    const imagen2 = document.querySelector(".modal-gallery__image-container")
+    imagen2.style.backgroundImage = " url(./images/image-product-2.jpg)";
+})
+
+foto3.addEventListener("click", function() {
+    const imagen3 = document.querySelector(".modal-gallery__image-container")
+    imagen3.style.backgroundImage = " url(./images/image-product-3.jpg)";
+})
+
+foto4.addEventListener("click", function() {
+    const imagen4 = document.querySelector(".modal-gallery__image-container")
+    imagen4.style.backgroundImage = " url(./images/image-product-4.jpg)";
+})
+
 // cerray Gallery DESKTOP
 
 cruz.addEventListener("click", function() {
